88ec60a353bdf97e53a3e486e642c5862de05c85
FIROKS <firoks1@gmail.com>
2020-10-13 23:49:24 +0300

    Подключает и реализует react-router-dom

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import MainScreen from '../main-screen/main-screen';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Offer from '../offer/offer';
 
 const App = (props) => {
   const {offersAmount} = props;
 
+  // return (
+  //   <MainScreen offersAmount = {offersAmount}/>
+  // );
   return (
-    <MainScreen offersAmount = {offersAmount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route path = '/' exact>
+          <MainScreen offersAmount = {offersAmount}></MainScreen>
+        </Route>
+        <Route path = '/login' exact>
+          <Login />
+        </Route>
+        <Route path = '/offer/:id?' exact>
+          <Offer />
+        </Route>
+        <Route path = '/favorites' exact>
+          <Favorites />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
